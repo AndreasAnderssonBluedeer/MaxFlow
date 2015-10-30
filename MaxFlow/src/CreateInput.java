629ff29fb23e87168b2b8c69c870c8964cc3b3d5
Andreas Andersson <bambismurfen@gmail.com>
2015-10-30 14:57:02 +0100

    Graph input, 2D array, skapar kopplingar i arrayen

---
+++ MaxFlow/src/CreateInput.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+
+/**
+ * Created by Andreas on 2015-10-30.
+ */
+public class CreateInput {
+
+    private int[][] array;
+    private int nodesLeft,nodesRight;
+
+    public CreateInput(){
+
+        nodesLeft= Integer.parseInt(JOptionPane.showInputDialog("Ange antal vänster noder."));
+        nodesRight= Integer.parseInt(JOptionPane.showInputDialog("Ange antal höger noder."));
+
+        array=new int [nodesLeft+nodesRight+2][nodesLeft+nodesRight+2];
+
+        //Definiera kopplingen från source till vänsternoder
+        for(int i=1; i<=nodesLeft;i++){
+            array[0][i]=1;
+        }
+
+        //Definera kopplingen från högernoder till sink.
+        for(int i=nodesLeft+1; i<array.length-1;i++){
+            array[i][array.length-1]=1;
+        }
+        boolean input=true;
+        // nodesLeft noder/värden = 1 till nodesleft value.
+        // nodesRight noder/värden= nodesleft+1 till nodesleft+nodesright.
+        while(input){
+            String left= (JOptionPane.showInputDialog("Skapa en connection från en av vänster noderna.\n"
+            +"En siffra från 1-"+nodesLeft+"\nTryck endast OK för att avsluta."));
+
+            String right= (JOptionPane.showInputDialog("Skapa en connection till en av höger noderna.\n"
+                    +"En siffra från "+(nodesLeft+1)+"-"+(nodesLeft+nodesRight)+"\nTryck endast OK för att avsluta."));
+
+            if(left.equals("") || right.equals("")){
+                input=false;
+            }
+            else{
+                int nodeLeft=Integer.parseInt(left);
+                int nodeRight=Integer.parseInt(right);
+                System.out.println("Gör connection från "+nodeLeft+" till "+nodeRight);
+
+                array[nodeLeft][nodeRight]=2;
+            }
+
+
+        }
+
+    }
+    public void printInput(){
+
+        for(int i=0; i<array.length;i++){
+            for(int k=0;k<array[i].length;k++){
+                System.out.print(array[i][k]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String [] args){
+        CreateInput ci=new CreateInput();
+        ci.printInput();
+    }
+}
---
+++ MaxFlow/src/Node.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Andreas on 2015-10-29.
+ */
+public class Node {
+
+}
