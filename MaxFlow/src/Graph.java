f5cc773651e721ece8602cac82cab1b3ce4be0b1
Andreas Andersson <bambismurfen@gmail.com>
2015-10-31 03:06:36 +0100

    funkar? node klassen...

--- MaxFlow/src/Graph.java
+++ MaxFlow/src/Graph.java
@@ -62,12 +62,14 @@ public class Graph {
         }
     }
 
-    public boolean bfs(int[][] graph, int source, int sink) {
-         boolean pathFound = false;
+    public boolean bfs(int[][] graph, int source, int sink) {   //returnera ifall det finns en väg
+
+        boolean pathFound = false;
         path = new int[graph.length];
         visited = new boolean[graph.length];
 
         for (int i = 0; i < graph.length; i++) {
+            path[i]=-1;
             visited[i] = false;
         }
         queue.add(source);
@@ -77,51 +79,88 @@ public class Graph {
         while (queue.size() != 0) {
             int element = queue.removeFirst();
 
-            for (int k = 0; k < graph.length; k++) {
+            for (int k = 1; k < graph.length; k++) {
                 if (!visited[k] && graph[element][k] > 0) {
                     visited[k] = true;
                     path[k] = element;
                     queue.add(k);
                 }
             }
-
-            if (visited[sink]) {
-                pathFound=true;
+        }   //?
+            if (visited[sink]) {    //Om vi nått sink-node finns det en väg.
+                pathFound=true;     //return true.
             }
-        }
-        return pathFound;
+      //  }
+        return pathFound;   //Returnera om det fanns en väg
     }
 
-    public int fordFulkerson(int[][] graph, int source, int sink) {
-        int f = 0;
+   // public int fordFulkerson(int[][] graph, int source, int sink) { //Hitta matchningar?
+     /*   int f = 0;
      //   while (bfs(graph, 0, graph.length - 1)) {
             while (bfs(graph, source,sink)) {
-            int fc = 2;
+            int flowCapacity = 200;
            // int v = path[graph.length - 1];
-            int v=sink;
-            while (v != source) {
+            int v=sink;     //sista index i arrayen. Sinkposition.
+            while (v != source) {   //v!= 0, dvs sourceposition/start
 
-                int u = path[v];
-                int min = graph[u][v];
-                fc = Math.min(fc, min);
-                v = path[v];
+                int u = path[v];    //u=sista värdet i path.
+                int min = graph[u][v];  //Hämta värdet 0/1
+                flowCapacity = Math.min(flowCapacity, min); //FC= det lägsta talet.
+                v = path[v];    //v= sista värdet i path. uppdatera loop
 
             }
-            v = path[graph.length - 1];
+           // v = path[graph.length - 1]; // v=path[sink] ??
+              //  v=path[sink];
+                v=sink;
+            while (v != source) { //v!=0 startnod
 
-            while (v != 0) {
-
-                int u = path[v];
-                graph[v][u] -= fc;
-                graph[u][v] += fc;
-                v = path[v];
+                int u = path[v];    //u=sista nod i path
+                graph[v][u] -= flowCapacity;    //Gör en förflyttning i grafen?
+                graph[u][v] += flowCapacity;    //Gör en förflyttning i grafen?
+                v = path[v];    //sista värdet i path...
             }
-            f += fc;
 
+            f += flowCapacity;  //f+ fc
+                System.out.println(f+" ?");
         }
 
 
-        return f;
+        return f;   //returnera antalet matches.*/
+   public int fordFulkerson(int[][] graph, int source, int sink) { //Hitta matchningar?
+
+        int u, v;
+        int maxFlow = 0;
+        int pathFlow;
+
+        int[][] residualGraph = new int[graph.length ][graph.length ];
+        for (int sourceVertex = 1; sourceVertex <graph.length; sourceVertex++)
+        {
+            for (int destinationVertex = 1; destinationVertex < graph.length; destinationVertex++)
+            {
+                residualGraph[sourceVertex][destinationVertex] = graph[sourceVertex][destinationVertex];
+            }
+        }
+
+        while (bfs(graph,source ,sink)) {
+            System.out.println("tja");
+            pathFlow = 2;
+            for (v = sink; v != source; v = path[v])
+            {
+                System.out.println("tja1");
+                u = path[v];
+                pathFlow = Math.min(pathFlow, residualGraph[u][v]);
+            }
+            for (v = sink; v != source; v = path[v])
+            {
+                System.out.println("tj2a");
+                u = path[v];
+                graph[u][v] -= pathFlow;
+                graph[v][u] += pathFlow;
+            }
+            maxFlow += pathFlow;
+        }
+
+        return maxFlow;
     }
 
     public static void main(String[] args) {
@@ -132,13 +171,13 @@ public class Graph {
         int[][] graph = {
                 {0,1,1,0,0,0},
                 {0,0,0,0,1,0},
-                {0,0,0,0,0,0},
+                {0,0,0,0,1,0},
                 {0,0,0,0,0,1},
-                {0,0,0,0,0,1},
+                {0,0,0,0,1,1},
                 {0,0,0,0,0,0}
         };
         g.printGraph(graph);
-        System.out.println("Maxflow för din graf är!:" + g.fordFulkerson(graph, 0, 5));
+        System.out.println("Maxflow för din graf är!: " + g.fordFulkerson(graph, 0, 5));
 
 
       //  Graph graph = new Graph();
